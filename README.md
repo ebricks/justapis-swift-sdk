a807f5952ac43170736d11ed2855df9a971bcc6e
Andrew Palumbo <apalumbo@anypresence.com>
2015-12-09 11:30:33 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JustAPIs Swift SDK
+
+A convenience library for communicating with a JustAPI's Gateway in Swift.
---
+++ Source/JustApisSwiftSDK.h
@@ -0,0 +1,19 @@
+//
+//  JustApisSwiftSDK.h
+//  JustApisSwiftSDK
+//
+//  Created by Andrew Palumbo on 12/9/15.
+//  Copyright © 2015 AnyPresence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JustApisSwiftSDK.
+FOUNDATION_EXPORT double JustApisSwiftSDKVersionNumber;
+
+//! Project version string for JustApisSwiftSDK.
+FOUNDATION_EXPORT const unsigned char JustApisSwiftSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JustApisSwiftSDK/PublicHeader.h>
+
+
